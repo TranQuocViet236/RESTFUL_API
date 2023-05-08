966cb6d422a3747040dae03f47b9b28541a13b9b
ai.vietqt <viet.tq@hahalolo.com>
2023-05-08 18:11:02 +0700

    format log

--- middleware.py
+++ middleware.py
@@ -1,6 +1,7 @@
 import logging
 from logging.handlers import TimedRotatingFileHandler
 from flask import request
+import json
 
 
 class LoggingMiddleware:
@@ -17,9 +18,38 @@ class LoggingMiddleware:
 
     def __call__(self, environ, start_response):
         def _start_response(status, headers, *args, **kwargs):
-            self.logger.info(f"Request: {request.method} {request.url} {status}")
-            self.logger.info(f"Request Headers: {request.headers}")
-            self.logger.info(f"Request Body: {request.get_data(as_text=True)}")
+            # print(status.get('code_str'))
+            # status_str = status
+            status_info = status.split(' ')
+            status_code = int(status_info[0])
+            status_msg = ' '.join(status_info[1:])
+
+            status_success = True if (status_msg == 'OK') else False
+            
+            status_obj = {
+                'success': status_success,
+                'code': status_code,
+            }
+
+            if not status_success:
+                status_obj.update({'errors': status_msg.lower()})
+            
+            bodyresp = {
+                'status': status_obj
+            }
+            
+            if status_success:
+                bodyresp.update({'elements': [request.get_data(as_text=True)]})
+            
+            bodyresp_str = json.dumps(bodyresp)
+            self.logger.info(f'BodyResp: {bodyresp_str}')
+            # self.logger.info(f'Status: {status}')
+            # print(type(status))
+            # print(status)
+            # self.logger.info(f"Request: {request.method} {request.url} {status}")
+            # self.logger.info(f"Request Headers: {request.headers}")
+            # self.logger.info(f"Request Body: {request.get_data(as_text=True)}")
+            
             return start_response(status, headers, *args, **kwargs)
 
         return self.app(environ, _start_response)
